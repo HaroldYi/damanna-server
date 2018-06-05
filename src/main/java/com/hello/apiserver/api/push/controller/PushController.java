e805e54e23b87bd9d9884a4d7e320a3957c5f1c2
Harold J. Yi <lji5317@me.com>
2018-06-06 00:37:28 +0900

    푸시보내기 방식 변경.

---
+++ src/main/java/com/hello/apiserver/api/push/controller/PushController.java
@@ -0,0 +1,73 @@
+package com.hello.apiserver.api.push.controller;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.hello.apiserver.api.util.PushNotificationsService.AndroidPushNotificationsService;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("/push")
+public class PushController {
+
+    @Autowired
+    AndroidPushNotificationsService androidPushNotificationsService;
+
+    @RequestMapping(value = "/sendPushMsg", method = RequestMethod.POST)
+    public ResponseEntity<String> sendPushMsg(
+            @RequestBody(required = false)String requestBody
+    ) {
+
+        Type mapType = new TypeToken<Map<String, String>>() {}.getType();
+        Map<String, String> map = new Gson().fromJson(requestBody, mapType);
+
+        JSONObject body = new JSONObject();
+        body.put("to", map.get("clientToken"));
+//        body.put("priority", "high");
+
+        JSONObject notification = new JSONObject();
+        notification.put("title", "");
+        notification.put("body", "");
+
+        JSONObject data = new JSONObject();
+        data.put("sayId", map.get("sayId"));
+        data.put("nofiMsg", map.get("nofiMsg"));
+        data.put("sortation", map.get("sortation"));
+
+        body.put("notification", notification);
+        body.put("data", data);
+
+        HttpHeaders headers = new HttpHeaders();
+        Charset utf8 = Charset.forName("UTF-8");
+        MediaType mediaType = new MediaType("application", "json", utf8);
+        headers.setContentType(mediaType);
+
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), headers);
+
+        CompletableFuture<String> pushNotification = androidPushNotificationsService.send(request);
+        CompletableFuture.allOf(pushNotification).join();
+
+        try {
+            String firebaseResponse = pushNotification.get();
+
+            return new ResponseEntity<>(firebaseResponse, HttpStatus.OK);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>("Push Notification ERROR!", HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/hello/apiserver/api/util/PushNotificationsService/AndroidPushNotificationsService.java
@@ -0,0 +1,38 @@
+package com.hello.apiserver.api.util.PushNotificationsService;
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+
+import com.hello.apiserver.api.util.HttpRequest.HeaderRequestInterceptor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class AndroidPushNotificationsService {
+
+    private static final String FIREBASE_SERVER_KEY = "AAAA51RpBMY:APA91bFn9W1LIVLMEzqRXxlPGCimN5EmxELSlvOJ7XVqhVZA1OVs2fafzpSwcJ5zODSSPNWhGJlasGb5axyZv4XgvzTZIDAQ3lbt8FhyPH9P0I2_dK2dgSrSl8wIyTGQ6blQ4YY8DixP";
+    private static final String FIREBASE_API_URL = "https://fcm.googleapis.com/fcm/send";
+
+    @Async
+    public CompletableFuture<String> send(HttpEntity<String> entity) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        /**
+         https://fcm.googleapis.com/fcm/send
+         Content-Type:application/json
+         Authorization:key=FIREBASE_SERVER_KEY*/
+
+        ArrayList<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new HeaderRequestInterceptor("Authorization", "key=" + FIREBASE_SERVER_KEY));
+        interceptors.add(new HeaderRequestInterceptor("Content-Type", "application/json"));
+        restTemplate.setInterceptors(interceptors);
+
+        String firebaseResponse = restTemplate.postForObject(FIREBASE_API_URL, entity, String.class);
+
+        return CompletableFuture.completedFuture(firebaseResponse);
+    }
+}
