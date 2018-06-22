d6dac2c2b9ed5dba0f6f729586bce3ece46b2911
Harold J. Yi <lji5317@me.com>
2018-06-22 16:40:09 +0900

    http응답 vo추가.

---
+++ src/main/java/com/hello/apiserver/api/util/commonVo/HttpResponseVo.java
@@ -0,0 +1,58 @@
+package com.hello.apiserver.api.util.commonVo;
+
+public class HttpResponseVo {
+    private String response = "";
+    private long timestamp;
+    private int status;
+    private String result = "";
+    private String message = "";
+    private String path = "";
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
