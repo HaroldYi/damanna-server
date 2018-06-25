2dc4683f0a9f7320d89414962bf941e76dd94e7b
Harold J. Yi <lji5317@me.com>
2018-06-25 09:25:14 +0900

    좋아요 임시.

--- src/main/java/com/hello/apiserver/api/like/controller/LikeController.java
+++ src/main/java/com/hello/apiserver/api/like/controller/LikeController.java
@@ -133,4 +133,62 @@ public class LikeController {
 
         return "";
     }
+
+    @RequestMapping(value = {"/meet/likeMeet/{sayId}/{memberId}", "/say/likeSay/{sayId}/{memberId}"}, method = RequestMethod.PUT)
+    public String likeSay1 (
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestHeader(value = "apiKey", required = false)String apiKey,
+            @PathVariable String sayId,
+            @PathVariable String memberId,
+            @PathVariable String clientToken
+    ) throws IOException {
+
+        if(Auth.checkApiKey(apiKey)) {
+            if (ObjectUtils.isEmpty(sayId)) {
+                response.sendError(HttpStatus.BAD_REQUEST.value(), "The request body must not be null or empty");
+            } else {
+                if(ObjectUtils.isEmpty(sayId)) {
+                    response.sendError(HttpStatus.BAD_REQUEST.value(), "The 'sayId' request body must not be null or empty");
+                } else {
+                    response.setStatus(HttpStatus.OK.value());
+
+                    String sortation = "";
+                    LikeSayVo likeSayVo = new LikeSayVo();
+                    MemberVo memberVo = this.memberRepository.findById(memberId);
+
+                    if(request.getRequestURI().indexOf("say") != -1) {
+                        likeSayVo = this.likeRepository.findBySayIdAndMemberAndUseYn(sayId, memberVo, "Y");
+                    } else if(request.getRequestURI().indexOf("meet") != -1) {
+                        likeSayVo = this.likeRepository.findByMeetIdAndMemberAndUseYn(sayId, memberVo, "Y");
+                    }
+
+                    if(likeSayVo != null) {
+                        this.likeRepository.delete(likeSayVo.getId());
+                    } else {
+                        likeSayVo = new LikeSayVo();
+
+                        if(request.getRequestURI().indexOf("say") != -1) {
+                            sortation = "S";
+                            likeSayVo.setSayId(sayId);
+                        } else if(request.getRequestURI().indexOf("meet") != -1) {
+                            sortation = "M";
+                            likeSayVo.setMeetId(sayId);
+                        }
+
+                        likeSayVo.setMember(memberVo);
+                        likeSayVo.setRegDt(new Date(Calendar.getInstance(TimeZone.getTimeZone("UTC")).getTimeInMillis()));
+                        likeSayVo.setUpdateDt(new Date(Calendar.getInstance(TimeZone.getTimeZone("UTC")).getTimeInMillis()));
+                        likeSayVo.setSortation(sortation);
+
+                        this.likeRepository.save(likeSayVo);
+                    }
+                }
+            }
+        } else {
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), "This api key is wrong! please check your api key!");
+        }
+
+        return "";
+    }
 }
