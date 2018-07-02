816e7bc126090c3fb9da526b92a235b26faa6926
Harold J. Yi <lji5317@me.com>
2018-07-02 18:19:31 +0900

    강퇴시 노티메시지 처리.

--- src/main/java/com/hello/apiserver/api/push/controller/PushController.java
+++ src/main/java/com/hello/apiserver/api/push/controller/PushController.java
@@ -25,11 +25,11 @@ public class PushController {
     @Autowired
     AndroidPushNotificationsService androidPushNotificationsService;
 
-    @RequestMapping(value = "/sendPushMsg/{os}", method = RequestMethod.POST)
+    @RequestMapping(value = "/sendPushMsg/{param}", method = RequestMethod.POST)
     public ResponseEntity<String> sendPushMsg(
             @RequestBody(required = false)String requestBody,
             @RequestHeader(value = "apiKey", required = false)String apiKey,
-            @PathVariable(value = "os") String os,
+            @PathVariable(value = "param") String param,
             HttpServletResponse response
     ) throws IOException {
 
@@ -42,40 +42,23 @@ public class PushController {
             body.put("priority", "high");
             body.put("content_available", true);
 
-//            JSONObject apns = new JSONObject();
-//            apns.put("title", "제주메이트");
-//            apns.put("badge", 1);
-//            apns.put("sound", "default");
-//
-//            body.put("apns", apns);
+            if(!ObjectUtils.isEmpty(param) && param.equals("leave")) {
+                JSONObject notification = new JSONObject();
+                notification.put("title", "제주메이트");
+                notification.put("body", map.get("notiMsg"));
+                notification.put("content_available", true);
+                notification.put("sound", "enabled");
 
+                body.put("notification", notification);
+            } else {
+                JSONObject data = new JSONObject();
+                data.put("sayId", map.get("sayId"));
+                data.put("notiMsg", map.get("notiMsg"));
+                data.put("sortation", map.get("sortation"));
+                data.put("content_available", true);
 
-//            if(!ObjectUtils.isEmpty(os) && os.equals("ios")) {
-//                JSONObject notification = new JSONObject();
-//                notification.put("title", "제주메이트");
-//                notification.put("body", map.get("notiMsg"));
-//                notification.put("content_available", true);
-//                notification.put("sound", "enabled");
-//
-//                body.put("notification", notification);
-//            }
-
-//            JSONObject android = new JSONObject();
-//            android.put("ttl", "86400s");
-//
-//            JSONObject notification1 = new JSONObject();
-//            notification1.put("click_action", "OPEN_ACTIVITY_1");
-//            android.put("notification", notification1);
-//
-//            body.put("android", android);
-
-            JSONObject data = new JSONObject();
-            data.put("sayId", map.get("sayId"));
-            data.put("notiMsg", map.get("notiMsg"));
-            data.put("sortation", map.get("sortation"));
-            data.put("content_available", true);
-
-            body.put("data", data);
+                body.put("data", data);
+            }
 
             HttpHeaders headers = new HttpHeaders();
             Charset utf8 = Charset.forName("UTF-8");
