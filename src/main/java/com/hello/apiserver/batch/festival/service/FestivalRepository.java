3bac20034895250bcd220703bec8711026f24cd3
Harold J. Yi <lji5317@me.com>
2018-08-09 10:17:41 +0900

    배치추가.

---
+++ src/main/java/com/hello/apiserver/batch/festival/service/FestivalRepository.java
@@ -0,0 +1,9 @@
+package com.hello.apiserver.batch.festival.service;
+
+import com.hello.apiserver.batch.festival.vo.FestivalVo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FestivalRepository extends CrudRepository<FestivalVo, Long> {
+    @Override
+    Iterable<FestivalVo> findAll(Iterable<Long> iterable);
+}
