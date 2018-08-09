7d1f36748deac8cfd1a2c7e345934de97d236767
Harold J. Yi <lji5317@me.com>
2018-08-09 18:14:47 +0900

    누락된 코드 추가.

--- src/main/java/com/hello/apiserver/api/comment/controller/CommentController.java
+++ src/main/java/com/hello/apiserver/api/comment/controller/CommentController.java
@@ -11,6 +11,7 @@ import com.hello.apiserver.api.util.Auth.Auth;
 import com.hello.apiserver.api.util.vo.HttpResponseVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
@@ -40,6 +41,7 @@ public class CommentController {
     ) throws IOException {
 
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+        boolean isError = false;
 
         HttpResponseVo httpResponseVo = new HttpResponseVo();
         httpResponseVo.setResponse("httpreponse");
@@ -58,13 +60,16 @@ public class CommentController {
             if (body == null || body.isEmpty()) {
                 httpResponseVo.setHttpResponse("The request body must not be null or empty", HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase());
                 httpStatus = HttpStatus.BAD_REQUEST;
+                isError = true;
             } else {
                 if(ObjectUtils.isEmpty(commentVo.getSayId()) && ObjectUtils.isEmpty(commentVo.getMeetId()) && ObjectUtils.isEmpty(commentVo.getFestivalId())) {
                     httpResponseVo.setHttpResponse("The 'sayId', 'meetId' or 'festivalId' request body must not be null or empty", HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase());
                     httpStatus = HttpStatus.BAD_REQUEST;
+                    isError = true;
                 } else if(ObjectUtils.isEmpty(commentVo.getComment())) {
                     httpResponseVo.setHttpResponse("The 'comment' request body must not be null or empty", HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase());
                     httpStatus = HttpStatus.BAD_REQUEST;
+                    isError = true;
                 } else {
                     response.setStatus(HttpStatus.OK.value());
                     commentVo.setRegDt(new Date(Calendar.getInstance(TimeZone.getTimeZone("UTC")).getTimeInMillis()));
@@ -85,17 +90,23 @@ public class CommentController {
                     }
 
                     commentVo.setSortation(sortation);
+                    commentVo = this.commentRepository.save(commentVo);
 
                     httpResponseVo.setHttpResponse("", HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase());
                     httpStatus = HttpStatus.OK;
                 }
             }
         } else {
+            isError = true;
             httpStatus = HttpStatus.UNAUTHORIZED;
             httpResponseVo.setHttpResponse("This api key is wrong! please check your api key!", HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
         }
 
-        return ResponseEntity.status(httpStatus).body(httpResponseVo);
+        if(isError) {
+            return ResponseEntity.status(httpStatus).body(httpResponseVo);
+        } else {
+            return ResponseEntity.status(httpStatus).contentType(MediaType.APPLICATION_JSON_UTF8).body(gson.toJson(commentVo));
+        }
     }
 
     @RequestMapping(value = {"/say/newCommentReply", "/meet/newCommentReply", "/festival/newCommentReply"}, method = RequestMethod.POST)
